e88fd921496343b0ba12802a880ee3a4ce4510c3
UTKARSHKUMAR24 <mkutkarsh07@gmail.com>
2024-03-12 16:15:56 +0530

    fixed bugs

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,35 +1,32 @@
 const HeroSection = () => {
-
-    return (
-        <main className="hero container">
-            <div className="hero=content">
-                <h1>YOUR FEET DESERVE THE BEST</h1>
-                <p>This is my first Shoe Shop Wev Page. 
-                    You can buy any type of Shoes from here whether it's sports shoes,
-                    school and office formal shoes or Informal Marrage Shoes.
-                </p>
-                <div className="hero-btn">
-                    <butoon>Shop Now</butoon>
-                    <butoon className = "secondary-btn">Category</butoon>
-                </div>
-
-                <div className="shopping">
-                    <p>Also Available On</p>
-
-                    <div className="brand-icons">
-                        <img src="/images/amazon.png" alt="amazon-logo"/>
-                        <img src="/images/flipkart.png" alt="flipkart-logo"/>
-                    </div>
-            </div>
-
-        <div className="hero-image">
-                <img src="../../public/images/shoe_image.png" alt="hero-image"></img>
+  return (
+    <main className="hero container">
+      <div className="hero-content">
+        <h1>YOUR FEET DESERVE THE BEST</h1>
+        <p>
+          This is my first Shoe Shop Wev Page. You can buy any type of Shoes
+          from here whether it's sports shoes, school and office formal shoes or
+          Informal Marrage Shoes.
+        </p>
+        <div className="hero-btn">
+          <butoon>Shop Now</butoon>
+          <butoon className="secondary-btn">Category</butoon>
         </div>
+
+        <div className="shopping">
+          <p>Also Available On</p>
+
+          <div className="brand-icons">
+            <img src="/images/amazon.png" alt="amazon-logo" />
+            <img src="/images/flipkart.png" alt="flipkart-logo" />
+          </div>
         </div>
-        </main>
+    </div>
 
-
-
-    );
+    <div className="hero-image">
+        <img src="../../public/images/shoe_image.png" alt="hero-image"></img>
+    </div>
+    </main>
+  );
 };
 export default HeroSection;
